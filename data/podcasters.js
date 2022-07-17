7c869e9ca394300080c2484a8a05679c5529b61e
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2022-07-17 21:20:02 +0400

    First 5 podcasters

---
+++ data/podcasters.js
@@ -0,0 +1,79 @@
+const podcasters = [
+    {
+        "title": "Rouben Mescian",
+        "keywords": [
+            "JavaScript",
+            "React",
+            "Node.js",
+            "Databases"
+        ],
+        "links": [
+            {
+                "type": "youtube",
+                "url": "https://www.youtube.com/user/meschian"
+            }
+        ]
+    },
+    {
+        "title": "Nairi Harutyunyan",
+        "keywords": [
+            "JavaScript",
+            "Node.js",
+            "DevOps",
+            "Databases"
+        ],
+        "links": [
+            {
+                "type": "youtube",
+                "url": "https://www.youtube.com/c/nairihar"
+            },
+            {
+                "type": "medium",
+                "url": "https://nairihar.medium.com/"
+            }
+        ]
+    },
+    {
+        "title": "Anushavan Yeghiazaryan",
+        "keywords": [
+            "JavaScript",
+            "HTML",
+            "CSS",
+            "SASS",
+            "SCSS"
+        ],
+        "links": [
+            {
+                "type": "youtube",
+                "url": "https://www.youtube.com/c/AnushavanYeghiazaryan"
+            }
+        ]
+    },
+    {
+        "title": "Sami Hayrapetyan",
+        "keywords": [
+            "HTML",
+            "CSS",
+            "Git"
+        ],
+        "links": [
+            {
+                "type": "youtube",
+                "url": "https://www.youtube.com/c/SamiHayrapetyan"
+            }
+        ]
+    },
+    {
+        "title": "CodeTubes",
+        "keywords": [
+            "Vue.js", "Angular.js", "Python",
+            "Docker", "Git"
+        ],
+        "links": [
+            {
+                "type": "youtube",
+                "url": "https://www.youtube.com/c/CodeTubes"
+            }
+        ]
+    }
+];
